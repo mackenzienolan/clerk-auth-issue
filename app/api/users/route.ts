247ad236ef5a4acca029a1c962ce08d24d11f3eb
Mackenzie Nolan <nolanmackenzie.a@gmail.com>
2024-07-18 08:44:06 -0400

    updated with passing url as well

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -4,12 +4,14 @@ export const dynamic = "force-dynamic"; // defaults to auto
 export async function GET(request: Request) {
   const clerkClient = createClerkClient({
     secretKey: process.env.CLERK_SECRET_KEY,
+    publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
   });
 
   const req = {
     headers: request.headers,
     method: request.method,
     body: request.body,
+    url: request.url,
   };
 
   const token = request.headers.get("Authorization")?.split(" ")[1];
